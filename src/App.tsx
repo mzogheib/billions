c6880e6cd2b3954feda3ff19a9eac348bb90be63
Marwan Zogheib <mzogheib@gmail.com>
2020-03-10 22:08:33 +1100

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React, { FC } from 'react'
-import { Grommet, Box } from 'grommet'
+import { Grommet } from 'grommet'
 import { grommet } from 'grommet/themes'
 
 import MainScreen from './screens/MainScreen'
 
 const App: FC = () => {
   return (
-    <Grommet theme={grommet} full>
-      <Box fill background="light-1">
-        <MainScreen />
-      </Box>
+    <Grommet theme={grommet} background="light-1" full>
+      <MainScreen />
     </Grommet>
   )
 }
--- src/screens/MainScreen/index.tsx
+++ src/screens/MainScreen/index.tsx
@@ -1,14 +1,46 @@
-import React, { FC } from 'react'
-import { Box, TextInput, Button } from 'grommet'
+import React, { FC, useState } from 'react'
+import { Box, TextInput, Button, Text } from 'grommet'
 
-const MainScreen: FC = () => (
-  <Box fill pad="large" justify="center" align="center">
-    Geenious
-    <Box background="white">
-      <TextInput placeholder="Search..." />
+const MainScreen: FC = () => {
+  const [searchTerm, setSearchTerm] = useState()
+
+  const handleInputChange = (e: any): void => {
+    const inputValue = e.target.value
+
+    if (inputValue && inputValue.length) {
+      setSearchTerm(inputValue)
+    } else {
+      setSearchTerm(undefined)
+    }
+  }
+
+  const handleSubmit = (e: any): void => {
+    e.preventDefault()
+    console.log(searchTerm)
+  }
+
+  const isButtonVisible = searchTerm && searchTerm.length
+
+  return (
+    <Box
+      fill
+      as="form"
+      pad="large"
+      gap="xlarge"
+      justify="center"
+      align="center"
+    >
+      <Text size="xxlarge">Geenious</Text>
+      <Box background="white">
+        <TextInput placeholder="Search..." onChange={handleInputChange} />
+      </Box>
+      <Box basis="xsmall" width="small">
+        {isButtonVisible && (
+          <Button primary type="submit" label="Go!" onClick={handleSubmit} />
+        )}
+      </Box>
     </Box>
-    <Button primary label="Go!" />
-  </Box>
-)
+  )
+}
 
 export default MainScreen
