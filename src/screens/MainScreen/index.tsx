84fce0af332ea1aaa9a1e358e62830d0a1249f07
Marwan Zogheib <mzogheib@gmail.com>
2020-03-11 08:09:07 +1100

    Add Form and functionality to MainScreen

--- src/screens/MainScreen/index.tsx
+++ src/screens/MainScreen/index.tsx
@@ -1,10 +1,10 @@
-import React, { FC, useState } from 'react'
-import { Box, TextInput, Button, Text } from 'grommet'
+import React, { FC, useState, FormEvent, ChangeEvent } from 'react'
+import { Box, TextInput, Button, Text, Form } from 'grommet'
 
 const MainScreen: FC = () => {
   const [searchTerm, setSearchTerm] = useState()
 
-  const handleInputChange = (e: any): void => {
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
     const inputValue = e.target.value
 
     if (inputValue && inputValue.length) {
@@ -14,7 +14,7 @@ const MainScreen: FC = () => {
     }
   }
 
-  const handleSubmit = (e: any): void => {
+  const handleSubmit = (e: FormEvent): void => {
     e.preventDefault()
     console.log(searchTerm)
   }
@@ -22,23 +22,18 @@ const MainScreen: FC = () => {
   const isButtonVisible = searchTerm && searchTerm.length
 
   return (
-    <Box
-      fill
-      as="form"
-      pad="large"
-      gap="xlarge"
-      justify="center"
-      align="center"
-    >
-      <Text size="xxlarge">Geenious</Text>
-      <Box background="white">
-        <TextInput placeholder="Search..." onChange={handleInputChange} />
-      </Box>
-      <Box basis="xsmall" width="small">
-        {isButtonVisible && (
-          <Button primary type="submit" label="Go!" onClick={handleSubmit} />
-        )}
-      </Box>
+    <Box fill pad="large" justify="center" align="center">
+      <Form onSubmit={handleSubmit}>
+        <Box gap="xlarge" align="center">
+          <Text size="xxlarge">Geenious</Text>
+          <Box background="white">
+            <TextInput placeholder="Search..." onChange={handleInputChange} />
+          </Box>
+          <Box basis="xsmall" width="small">
+            {isButtonVisible && <Button primary type="submit" label="Go!" />}
+          </Box>
+        </Box>
+      </Form>
     </Box>
   )
 }
