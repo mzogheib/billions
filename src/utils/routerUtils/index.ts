a1e5491d6917e718901ae721a8b2196be185ada1
Marwan Zogheib <mzogheib@gmail.com>
2020-04-05 17:08:22 +1000

    Add a makeQueryParams function

--- src/utils/routerUtils/index.ts
+++ src/utils/routerUtils/index.ts
@@ -1,4 +1,8 @@
 import { useLocation } from 'react-router-dom'
-import { parse, ParsedQuery } from 'query-string'
+import { parse, ParsedQuery, stringify } from 'query-string'
 
 export const useQuery = (): ParsedQuery => parse(useLocation().search)
+
+export const makeQueryParams = (params: {
+  [key: string]: string | number | boolean
+}): string => stringify(params)
