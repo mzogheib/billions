98a170702b11137d259ce888e2c4f54a36c46d2e
Marwan Zogheib <mzogheib@gmail.com>
2020-04-11 15:27:48 +1000

    Redirect to main screen on /oauth route error

--- src/screens/OAuth/index.tsx
+++ src/screens/OAuth/index.tsx
@@ -1,14 +1,17 @@
 import React, { FC } from 'react'
 import { Box } from 'grommet'
+import { useHistory } from 'react-router-dom'
 
 import { onOAuthResponse, GeniusOAuthResponse } from '../../services/genius'
 import { useQuery } from '../../utils/routerUtils'
 
 const OAuth: FC = () => {
   const parsedQueryParams = useQuery({ hash: true }) as GeniusOAuthResponse
+  const { push } = useHistory()
 
   const handleError = (message: string): void => {
     console.error(message)
+    push(`/`)
   }
 
   onOAuthResponse(parsedQueryParams, handleError)
