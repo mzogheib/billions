beaadaa7d192a83aabade9ae1f8a1062db732230
Marwan Zogheib <mzogheib@gmail.com>
2020-04-19 19:21:59 +1000

    Add SearchResultsList

---
+++ src/screens/Search/SearchResultsList/index.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react'
+import { Box } from 'grommet'
+
+import SearchResult, {
+  Props as SearchResultInterface,
+} from '../../../components/SearchResult'
+
+// Need an id to add as the key for each list item
+export type SearchResults = (SearchResultInterface & { id: number })[]
+
+interface Props {
+  results: SearchResults
+}
+
+const SearchResultsList: FC<Props> = ({ results }: Props) => (
+  <Box pad="medium" gap="medium">
+    {results.map(({ id, type, title, imageUrl }) => (
+      <SearchResult key={id} type={type} title={title} imageUrl={imageUrl} />
+    ))}
+  </Box>
+)
+
+export default SearchResultsList
