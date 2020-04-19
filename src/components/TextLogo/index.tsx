8a2a33f951eee49a2a01ed7f2538b965e078fe4d
Marwan Zogheib <mzogheib@users.noreply.github.com>
2020-04-19 14:40:46 +1000

    feat: add custom font for logo (#18)
    * Add FredokaOne font
    
    * Add new font to TextLogo

--- src/components/TextLogo/index.tsx
+++ src/components/TextLogo/index.tsx
@@ -1,5 +1,10 @@
 import React, { FC } from 'react'
 import { Text } from 'grommet'
+import styled from 'styled-components'
+
+const Wrapper = styled(Text)`
+  font-family: 'FredokaOne';
+`
 
 interface Props {
   size?: string
@@ -8,9 +13,9 @@ interface Props {
 // Feels redundant to specify the type twice but here is some background as to why
 // https://github.com/yannickcr/eslint-plugin-react/issues/2353
 const TextLogo: FC<Props> = ({ size }: Props) => (
-  <Text size={size} color="brand">
+  <Wrapper size={size} color="brand">
     Geenious
-  </Text>
+  </Wrapper>
 )
 
 export default TextLogo
