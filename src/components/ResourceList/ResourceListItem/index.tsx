463716bce9737da1009c0889cb83c8d3eba0e5df
Marwan Zogheib <mzogheib@gmail.com>
2020-09-05 14:13:06 +1000

    Add ResourceListItem

---
+++ src/components/ResourceList/ResourceListItem/index.tsx
@@ -0,0 +1,53 @@
+import React, { FC } from 'react'
+import { Text, Box, Image } from 'grommet'
+import { Disc as DiscIcon } from 'grommet-icons'
+
+import LoadingBox from '../../../components/LoadingBox'
+
+export interface DataProps {
+  imageUrl?: string
+  title: string
+}
+
+interface FunctionProps {
+  onSelect: () => void
+}
+
+type Props = DataProps & FunctionProps
+
+const ResourceListItem: FC<Props> = ({ imageUrl, title, onSelect }: Props) => (
+  <Box
+    background="white"
+    pad="medium"
+    gap="small"
+    round="medium"
+    direction="row"
+    align="center"
+    onClick={onSelect}
+  >
+    <Box flex="grow" width={{ max: 'xxsmall' }} height="xxsmall">
+      {imageUrl ? (
+        <Image src={imageUrl} fit="contain" />
+      ) : (
+        <DiscIcon size="large" />
+      )}
+    </Box>
+    <Text truncate={true}>{title}</Text>
+  </Box>
+)
+
+export default ResourceListItem
+
+export const ResourceListItemPlaceholder: FC = () => (
+  <Box
+    background="white"
+    pad="medium"
+    gap="small"
+    round="medium"
+    direction="row"
+    align="center"
+  >
+    <LoadingBox width="xxsmall" />
+    <LoadingBox flex="grow" height="24px" />
+  </Box>
+)
